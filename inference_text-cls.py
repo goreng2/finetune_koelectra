1193bdf129207f10c1e369b07fb27adefa0f33b0
박정운 <duqrlwjddns1@naver.com>
2022-05-24 16:53:49 +0900

    id2label 추가

--- inference_text-cls.py
+++ inference_text-cls.py
@@ -3,29 +3,29 @@ import time
 import torch
 
 
-path = "result/koelectra-nsmc"
+path = "result/nsmc"
 tokenizer = AutoTokenizer.from_pretrained(path)
-model = AutoModelForSequenceClassification.from_pretrained(path)
+model = AutoModelForSequenceClassification.from_pretrained(path, id2label={0: "부정", 1: "긍정"})
 classifier = pipeline(task="text-classification", model=model, tokenizer=tokenizer)
 
-torchscript_model = torch.jit.load("torchscript_electra.pt").eval()
-optimized_model = torch.jit.load("optimized_torchscript_electra.pth").eval()
+# torchscript_model = torch.jit.load("torchscript_electra.pt").eval()
+# optimized_model = torch.jit.load("optimized_torchscript_electra.pth").eval()
 
 while True:
     text = input("input: ")
-    tensors = tokenizer(text, return_tensors="pt")["input_ids"]
 
     start = time.time()
     result = classifier(text)
-    end = time.time() - start
-    print("[original]", result, "{0:.2f}".format(end))
+    latency = time.time() - start
+    print(result, "{0:.2f}".format(latency))
 
-    start = time.time()
-    result = torchscript_model(tensors)
-    end = time.time() - start
-    print("[torchscript]", result, "{0:.2f}".format(end))
-
-    start = time.time()
-    result = optimized_model(tensors)
-    end = time.time() - start
-    print("[Quantize]", result, "{0:.2f}".format(end))
+    # tensors = tokenizer(text, return_tensors="pt")["input_ids"]
+    # start = time.time()
+    # result = torchscript_model(tensors)
+    # end = time.time() - start
+    # print("[torchscript]", result, "{0:.2f}".format(end))
+    #
+    # start = time.time()
+    # result = optimized_model(tensors)
+    # end = time.time() - start
+    # print("[Quantize]", result, "{0:.2f}".format(end))
